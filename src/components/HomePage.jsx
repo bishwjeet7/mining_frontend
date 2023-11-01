db65416b17ac3c800cf2f40d76eb79622c680841
Bishwjeet kumar <66054602+bishwjeet7@users.noreply.github.com>
2023-11-01 13:54:17 +0530

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,11 @@
 // HomePage.js
 import React, { useEffect, useState } from 'react';
 import ncl from '../ncl.jpg';
-import iit from '../iit_logo_originals.png';
+import iit from '../iit_logo_original.png';
 import '../../src/App.css';
 import { Navigate } from 'react-router-dom';
+import '../App.css'
+import homepage_logo from '../homepage.jpg'
 
 const HomePage = () => {
   const [shouldRedirect, setShouldRedirect] = useState(false);
@@ -11,24 +13,25 @@ const HomePage = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShouldRedirect(true);
-    }, 5000);
+    }, 500000);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div>
-      <h1>IDR Application</h1>
-      <div className='imgBox'>
-        <img src={iit} alt='iit'></img>
-        <img src={ncl} alt='ncl'></img>
-      </div>
-      <div className='details'>
-        <h3>Project PI - Prof Sk Sharma</h3>
-        <h3>Research Fellow - Kadam Mishra</h3>
-      </div>
-      {shouldRedirect && <Navigate to="/graph" />}
-    </div>
+    <div className='homePageLogo' style={{ backgroundImage: `url(${homepage_logo})` }}>
+  <h1>TDR Application</h1>
+  <div className='imgBox'>
+    <img src={iit} alt='iit'></img>
+    <img src={ncl} alt='ncl'></img>
+  </div>
+  <div className='details'>
+    <h3>Project PI - Prof Sk Sharma</h3>
+    <h3>Research Fellow - Kadam Mishra</h3>
+  </div>
+  {shouldRedirect && <Navigate to="/graph" />}
+</div>
+
   );
 }
 
